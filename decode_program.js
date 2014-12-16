9af41ca6d29352fab89a0cba6aa02221cc794cb4
Fiona Liao <fl2213@ic.ac.uk>
2014-12-16 21:36:43 +0000

    Decode now working.

--- decode_program.js
+++ decode_program.js
@@ -1,8 +1,8 @@
 var decode = function(toDecode){
-		return decodeList(toDecode);
+		return decodeList(toDecode, 0);
 	};
 	
-var decodeList = function(list) {
+var decodeList = function(list, index) {
 	var head = 0;
 	while (list != 0 && list % 2 == 0) {
 		head++;
@@ -11,10 +11,51 @@ var decodeList = function(list) {
 	if (list == 0) {
 		return "";
 	}
-	return decodeInstruction(head) + decodeList((list - 1)/2);
+	nextIndex = index + 1;
+	return decodeInstruction(head, index) + decodeList((list - 1)/2, nextIndex);
 };
 
-var decodeInstruction = function(instruction) {
-	//TODO
+var decodeInstruction = function(instruction, index) {
+	if (instruction == 0) {
+		return "<br/>" + writeHaltInstr(index); 
+	}
+	var x = 0;
+	while (instruction % 2 == 0) {
+		x++;
+		instruction = instruction / 2;
+	}
+	instruction = ((instruction-1)/2);
+	if (x % 2 == 0) { //increment instruction
+		return "<br/>" + writeIncInstr(index, (x/2), instruction);
+	} else { //decrement instruction
+		instruction++;
+		var j = 0;
+		var k = 0;
+		j = calculateGreatestPowerOfTwo(instruction);
+		k = ((instruction / Math.pow(2, j)) - 1) / 2;
+		return "<br/>" + writeDecInstr(index, ((x-1)/2), j, k);
+	}
 	return instruction + "; ";
 };
+
+var calculateGreatestPowerOfTwo = function(n) {
+	var powerOfTwo = 0;
+	while (n != 0 && n % 2 == 0) {
+		powerOfTwo++;
+		n = n / 2;
+	}
+	return powerOfTwo;
+};
+
+var writeIncInstr = function(instrNo, regNo, nextInstrNo) {
+    return "L<sub>" + instrNo + "</sub>: R<sub>" + regNo + "</sub><sup>+</sup> &#8594; L<sub>" + nextInstrNo + "</sub>;";
+};
+
+var writeDecInstr = function(instrNo, regNo, trueNextInstrNo, falseNextInstrNo) {
+    return "L<sub>" + instrNo + "</sub>: R<sub>" + regNo + "</sub><sup>-</sup> &#8594; L<sub>" + trueNextInstrNo + "</sub>,L<sub>" + falseNextInstrNo + "</sub>;";
+};
+
+var writeHaltInstr = function(instrNo) {
+	return "L<sub>" + instrNo + "</sub>: HALT;";
+};
+
