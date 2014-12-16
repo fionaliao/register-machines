79b7bae040685f714b16fe9d57c6f798868aedc9
Fiona Liao <fl2213@ic.ac.uk>
2014-12-16 20:55:14 +0000

    Completed list decoding.

--- decode_program.js
+++ decode_program.js
@@ -1,1 +1,20 @@
-var decode = function(toDecode){};
+var decode = function(toDecode){
+		return decodeList(toDecode);
+	};
+	
+var decodeList = function(list) {
+	var head = 0;
+	while (list != 0 && list % 2 == 0) {
+		head++;
+		list = list / 2;
+	}
+	if (list == 0) {
+		return "";
+	}
+	return decodeInstruction(head) + decodeList((list - 1)/2);
+};
+
+var decodeInstruction = function(instruction) {
+	//TODO
+	return instruction + "; ";
+};
