8b8d74b4e90504fd8360575d15dd95e9d2ad68fb
Gabriel Cowley <gabrielcowley@Gabriels-MacBook-Pro.local>
2014-12-16 22:38:05 +0000

    Completed encoding without error checking

--- encode_program.js
+++ encode_program.js
@@ -4,33 +4,81 @@ var PPair = function (x, y, type) {
     this.type = type;
 }
 
-var SPair = function (x, y) {
-    this.x = x;
-    this.y = y;
+function convertSPairToValue(x, y) {
+    var fst = Math.pow(2, x);
+    var snd = (2 * y) + 1;
+    return (fst * snd) - 1;
+}
+
+function convertPPairToValue(pPair) {
+    if (pPair.type == 0) {
+        var fst = Math.pow(2, pPair.x);
+        var snd = (2 * pPair.y) + 1;
+        return fst * snd;
+    }
+    return 0;
+}
+
+function encodeIntoPPairs(toEncodeArray) {
+    var result = new Array(toEncodeArray.length);
+    for (var i = 0; i < toEncodeArray.length; i++) {
+        if (toEncodeArray[i].indexOf("+") > -1) {
+            var fstString = toEncodeArray[i].split("+")[0];
+            //console.log(toEncodeArray[i].split("+"));
+            var fst = parseInt(fstString);
+            //console.log("PPair fst: " + fst);
+            var sndString = toEncodeArray[i].split(">")[1];
+            var snd = parseInt(sndString);
+            result[i] = new PPair(2 * fst, snd, 0);
+        } else if (toEncodeArray[i].indexOf("-") > -1) {
+            var fstString = toEncodeArray[i].split("-")[0];
+            //console.log(toEncodeArray[i].split("-"));
+            var fst = parseInt(fstString);
+            //console.log("PPair fst: " + fst);
+            var sndString = toEncodeArray[i].split(">")[1];
+            var sndFstString = sndString.split(",")[0];
+            var sndFst = parseInt(sndFstString);
+            //console.log("SPair fst: " + sndFst);
+            var sndSndString = sndString.split(",")[1];
+            var sndSnd = parseInt(sndSndString);
+            //console.log("SPair snd: " + sndSnd);
+            var snd = convertSPairToValue(sndFst, sndSnd);
+            //console.log("SPair result: " + snd);
+            result[i] = new PPair((2 * fst) + 1, snd, 0);
+        } else {
+            result[i] = new PPair(undefined, undefined, 1);
+        }
+    }
+    return result;
+}
+
+function encodeIntoValues(encodedPPairs) {
+    var result = new Array(encodedPPairs.length + 1);
+    for (var i = 0; i < encodedPPairs.length; i++) {
+        //console.log("PPair: <" + encodedPPairs[i].x + ", " + encodedPPairs[i].y + ">");
+        result[i] = convertPPairToValue(encodedPPairs[i]);
+    }
+    result[encodedPPairs.length] = 0;
+    return result;
+}
+
+function encodeIntoSingleValue(encodedValues) {
+    for (var i = encodedValues.length - 2; i >= 0; i--) {
+        var fst = Math.pow(2, encodedValues[i]);
+        var snd = (2 * encodedValues[i + 1]) + 1;
+        encodedValues[i] = fst * snd;
+        console.log(fst * snd);
+    }
+    return encodedValues[0];
 }
 
 var encode = function(toEncode) {
     var toEncodeArray = toEncode.split(";");
-    var encodedPPairs = new Array(toEncodeArray.length);
-    for (var i = 0; i < toEncodeArray.length; i++) {
-        if (toEncodeArray[i].indexOf("+") > -1) {
-            var fstString = toEncodeArray[i].split("+")[0];
-            var fst = parseInt(fstString);
-            var sndString = toEncodeArray[i].split(">")[1];
-            var snd = parseInt(sndString);
-            encodedPPairs[i] = new PPair(fst, snd, 0);
-        } else if (toEncodeArray[i].indexOf("-") > -1) {
-            var fstString = toEncodeArray[i].split("-")[0];
-            var fst = parseInt(fstString);
-            var sndString = toEncodeArray[i].split(">")[1];
-            var sndFstString = sndString.split(",")[0];
-            var sndFst = parseInt(sndFstString);
-            var sndSndString = sndString.split(",")[1];
-            var sndSnd = parseInt(sndSndString);
-            var snd = new SPair(sndFst, sndSnd);
-            encodedPPairs[i] = new PPair(fst, snd, 1);
-        } else {
-            encodedPPairs[i] = new PPair(undefined, undefined, 2);
-        }
-    }
+    var encodedPPairs = encodeIntoPPairs(toEncodeArray);
+    var encodedValues = encodeIntoValues(encodedPPairs);
+    /*console.log("Encoded values:");
+    for (var i = 0; i < encodedValues.length; i++) {
+        console.log(encodedValues[i]);
+    }*/
+    return encodeIntoSingleValue(encodedValues);
 };
